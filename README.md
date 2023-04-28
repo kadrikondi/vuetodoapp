a2d8f5588011ac1ff5bb41287f100b5cbf78e2f9
kadrikondi <abdulkadri42@gmail.com>
2023-04-28 06:46:11 +0100

    vue todo app

---
+++ README.md
@@ -0,0 +1,40 @@
+# Todo App
+
+A Todo App that you can list your activities and mark them as Complete or Incomplete. It remembers To-do's even after your PC shuts down.
+
+![App Showcase](/images/todo_demo.gif "App Demo")
+
+## Features
+- Self Explanatory User Interface (UI).
+- Mark To-do's Complete or Incomplete.
+- Remembers even after browser gets accidentally closed or restarted.
+- Even remember after your PC shuts down.
+
+
+
+## Screenshots
+![App Showcase](/images/screenshots.png "App Showcase")
+
+
+## Project Details
+Created using <strong>VueJS 3</strong> and <strong>TailwindCSS</strong>.
+
+
+## Setting up the Project
+
+#### Clone the Repository
+```
+git clone git@github.com:sayansinha5/VueJS-3-TodoList.git
+```
+
+#### Install the NPM Packages
+```
+npm install
+```
+
+#### Run
+```
+npm run serve
+```
+
+#### Open in Browser: [http://localhost:8080](http://localhost:8080)
---
+++ src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container mx-auto px-10 md:px-0">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div></div>
+      <div>
+        <div class="mt-10">
+          <h1 class="text-5xl font-semibold text-gray-600">Todo List</h1>
+
+          <div class="mt-6">
+            <form @submit.prevent="addTodo()">
+              <div class="grid grid-cols-1 gap-4">
+                <div>
+                  <input class="border-2 outline-none py-2 px-2 shadow-md font-medium w-full rounded border-blue-300 hover:border-blue-600 focus:border-blue-600 focus:shadow-blue-200" v-model="newTodo" />
+                </div>
+                <div>
+                  <div class="grid grid-cols-2 gap-2">
+                    <div>
+                      <button class="text-white py-2 px-4 shadow-md w-full rounded bg-red-400 hover:bg-red-600 font-semibold" v-if="todos.length !== 0" @click="removeAllTodos">Remove All Todos</button>
+                    </div>
+                    <div>
+                      <button class="text-white py-2 px-4 shadow-md w-full rounded bg-blue-400 hover:bg-blue-600 font-semibold">Add Todo</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div class="mt-8 text-center">
+            <div id="todoList">
+              <div class="grid grid-cols-1 gap-3">
+                <div>
+                  <div class="h-full" v-if="todos.length === 0">
+                    <p class="text-gray-400">It appears you didn't added any To-Do.</p>
+                  </div>
+                </div>
+                <div class="rounded shadow-md p-3 h-full hover:shadow-gray-400 text-gray-600 text-lg font-semibold" :class="{ completed: todo.complete }" v-for="(todo, index) in todos" :key="index" @click="completedTodo(todo)">
+                  <span class="text-gray-600 text-lg font-semibold">{{ todo.text }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div></div>
+    </div>
+  </div>
+  <footer class="text-center">
+    with <span class="h"> &hearts;</span> by Kondipress {{ year }}
+  </footer>
+</template>
+
+<script setup>
+let dat = new Date()
+
+let year =dat.getFullYear()
+console.log(year )
+
+  import { ref } from "vue";
+  
+  const newTodo = ref("");
+  // const initialData = [
+  //   {
+  //     complete: false,
+  //     text: "Create todo list functionality"
+  //   }
+  // ];
+  
+  let storedTodos;
+  localStorage.getItem("todos") ? storedTodos = JSON.parse(localStorage.getItem("todos")) : (storedTodos = []);
+  
+  const todos = ref(storedTodos);
+  
+  function addTodo() {
+    
+    if(newTodo.value !== "") {
+      todos.value.push({ complete: false, text: newTodo.value });
+      newTodo.value = "";
+    }
+  }
+  
+  function removeAllTodos() {
+    todos.value.splice(0, todos.value.length);
+    updateStorage();
+  }
+  
+  function completedTodo(todo) {
+    todo.complete = !todo.complete;
+    updateStorage();
+  }
+  
+  function updateStorage() {
+    localStorage.setItem('todos', JSON.stringify(todos.value));
+  }
+</script>
+
+<style>
+  .completed {
+    text-decoration: line-through;
+  }
+  .h{
+    color: red;
+    font-size: 1.5em;
+  }
+</style>
